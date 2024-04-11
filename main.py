6809f9db943d986bede7aab87e5e732ab38601e8
ptorres29593 <123116819+ptorres29593@users.noreply.github.com>
2024-04-10 20:37:09 -0600

    Add: Fitness

--- main.py
+++ main.py
@@ -29,19 +29,38 @@ def gen_distances():
     return distances
 
 
-def gen_random_journey(len):
+def gen_random_journey(length):
     cities = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-    selected_cities = cities[:len]
+    selected_cities = cities[:length]
     random_journey = np.array(selected_cities)
     np.random.shuffle(random_journey)
 
     return random_journey
 
 
-# def fitness(journey, distances):
+def fitness(journey, distances):
+    cities = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+              'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+    indexes = []
+
+    for movement in journey:
+        for i, city in enumerate(cities):
+            if movement == city:
+                indexes.append(i)
+
+    distance = 0
+
+    for i, index in enumerate(indexes):
+
+        if i == len(indexes) - 1:
+            distance += distances[i][indexes[0]]
+        else:
+            distance += distances[index][indexes[i + 1]]
+
+    return distance
 
 
 def evolutionary_algorithm(population_size, num_cities):
@@ -52,4 +71,5 @@ def evolutionary_algorithm(population_size, num_cities):
 
 
 if __name__ == '__main__':
-    print(evolutionary_algorithm(10, 15))
+    #    print(evolutionary_algorithm(10, 15))
+    print(fitness(gen_random_journey(4), gen_distances()))
