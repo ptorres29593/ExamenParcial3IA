53dd980ab04dfb3d004d4c22cbb5dc3b0e144fea
ptorres29593 <123116819+ptorres29593@users.noreply.github.com>
2024-04-10 19:25:17 -0600

    Add: First Changes

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def gen_distances():
+    distances = [[0, 5, 14, 30],
+                 [5, 0, 10, 9],
+                 [14, 10, 0, 20],
+                 [30, 9, 20, 0], ]
+
+    # distances = [[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    #               [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    #               [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    #               [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    #               [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    #               [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    #               [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    #               [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    #               [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    #               [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+    #               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+    #               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+    #               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    #               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    #               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],]
+
+    # for i in range(16):
+    #     for j in range(16):
+
+    return distances
 
 
-# Press the green button in the gutter to run the script.
+def gen_random_journey(len):
+    cities = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+              'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    selected_cities = cities[:len]
+    random_journey = np.array(selected_cities)
+    np.random.shuffle(random_journey)
+
+    return random_journey
+
+
+# def fitness(journey, distances):
+
+
+
+def evolutionary_algorithm(population_size, num_cities):
+    distances = gen_distances()
+    population = np.array([gen_random_journey(num_cities) for _ in range(population_size)])
+    # a = gen_random_journey(4)
+    return population
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(evolutionary_algorithm(10, 15))
